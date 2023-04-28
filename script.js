57616f72ca0169f43b4cdd3bb81a356cc58b4404
Diabetinho <brendon_poa@hotmail.com>
2023-04-28 16:20:16 -0300

    bug fixes in script and html and css changes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
 const highScoreElement = document.querySelector(".high-score");
-const controls = document.querySelector(".controls i");
+const controls = document.querySelectorAll(".controls i");
 
 let gameOver = false;
 let foodX, foodY;
 let snakeX = 5, snakeY = 5;
 let velocityX = 0, velocityY = 0;
-let snakeBody = []
+let snakeBody = [];
 let setIntervalId;
 let score = 0;
 
@@ -25,46 +25,44 @@ const handleGameOver = () => {
     location.reload();
 }
 
-const changeDirection = e =>{
-    if(e.key === "ArrowUp" && velocityY != 1){
+const changeDirection = e => {
+    if (e.key === "ArrowUp" && velocityY != 1){
         velocityX = 0;
         velocityY = -1;
-    }else if(e.key === "ArrowDown" && velocityY != -1){
+    } else if (e.key === "ArrowDown" && velocityY != -1){
         velocityX = 0;
-        velocityY = -1;       
-    }else if(e.key === "ArrowLeft" && velocityX != 1){
+        velocityY = 1;       
+    } else if (e.key === "ArrowLeft" && velocityX != 1){
         velocityX = -1;
         velocityY = 0;
-    }else if(e.key === "ArrowRight" && velocityX != -1){
+    } else if (e.key === "ArrowRight" && velocityX != -1){
         velocityX = 1;
         velocityY = 0;
     }
 }
 
-controls.forEach(button => button.addEventListener("click", () => changeDirection({
-    key: button.dataset.key })));
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
 const initGame = () => {
-    if(gameOver) return handleGameOver();
-    let html = `<div class="food style="grid-area: ${foodY} / ${foodX}=></div>`;
+    if (gameOver) return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
-    if(snakeX === foodX && snakeY === foodY){
+    if (snakeX === foodX && snakeY === foodY) {
         updateFoodPosition();
         snakeBody.push([foodY, foodX]);
         score++;
         highScore = score >= highScore ? score : highScore;
 
-        localStorage.setItem("high-Score", highScore);
+        localStorage.setItem("high-score", highScore);
         scoreElement.innerText = `Score: ${score}`;
-        highScoreElement.innerText = `High Score ${highScore}`;
-        
+        highScoreElement.innerText = `High Score: ${highScore}`; 
     }
 
     snakeX += velocityX;
     snakeY += velocityY;
 
-    for(let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody [i - 1];
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
     }
 
     snakeBody[0] = [snakeX, snakeY];
@@ -73,8 +71,8 @@ const initGame = () => {
         return gameOver = true;
     }
 
-    for(let i = 0; i < snakeBody.length; i++){
-        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody [i][0]}"</div>`;
+    for (let i = 0; i < snakeBody.length; i++) {
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
         if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
             gameOver = true;
         }
